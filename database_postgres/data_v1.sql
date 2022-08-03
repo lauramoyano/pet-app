6f8153263a53ce2239f27f136084a2e6f7241429
lauramoyano <laura.moyano.go@gmail.com>
2022-08-03 14:17:58 -0500

    First commit!

--- database_postgres/data_v1.sql
+++ database_postgres/data_v1.sql
@@ -1,5 +1,5 @@
 \c mande_db
 
 INSERT INTO type_(dtype, breed, type_description) VALUES ('cat', 'Bengala', 'It looks like fire');
-INSERT INTO  pet(pet_name, age, typeid) VALUES ('Angel', 70, 1);
-INSERT INTO toy(toy_name, color, pet_id) VALUES('bola1', 'red', 1);
+INSERT INTO  pet(pet_name, age, typeid) VALUES ('Max', 70, 1);
+INSERT INTO toy(toy_name, color, pet_id) VALUES('pelota', 'red', 1);
